24ef82723c74abfa51ce01e144a8b5071e4ffd07
Florin <furtuna.florin90@gmail.com>
2019-01-15 16:12:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function sayHiToGrandma(string)  {
   if (string.toUpperCase()===string)  {
    return "YES INDEED!"
   }
-  var 
+  var string = "I love you, Grandma";
   if (string()==="I love you, Grandma."){
     return "I love you, too."
   }
